b909dd480ddaa8e495c806611332e2084886a42d
Patrick Bertsch <47899331+pat-bert@users.noreply.github.com>
2020-11-24 15:30:34 +0100

    Calculate fill factor of contours

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import random
 from glob import glob
 
 import cv2
@@ -5,12 +6,19 @@ import numpy as np
 
 from check_label import ROOT_PATH
 
+# Skin Color Mask
 LMIN, LMAX = 0, 255
 AMIN, AMAX = 131, 165
-BMIN, BMAX = 132, 178
-BLUR_SIZE = 3
-EROSION_SIZE = 9
-closing_size = 3
+BMIN, BMAX = 132, 190
+
+# Filter Kernel Sizes
+CANNY_BLUR_SIZE = 5
+BINARY_OPEN_CLOSE_SIZE = 9
+EDGE_DILATION_SIZE = 3
+MIN_FILL_RATIO = 0.3
+
+# Valid Area Range
+MIN_AREA_REL, MAX_AREA_REL = 0.03, 0.7
 
 
 def make_uneven(val):
@@ -18,14 +26,38 @@ def make_uneven(val):
     return val if val % 2 == 1 else val + 1
 
 
+def get_area_thresholds(image):
+    """
+    Calculate the absolute area thresholds with respect to the total image area
+    :param image:
+    :return:
+    """
+    total_area = np.prod(image.shape[0:2])
+    return MIN_AREA_REL * total_area, MAX_AREA_REL * total_area
+
+
+def do_nothing():
+    pass
+
+
 if __name__ == '__main__':
-    # Get a random image from the set of allowed labels
-    image_files = glob(str(ROOT_PATH) + '/images_320-240_1/*/*/Color/*/*.jp*g')
+    # Get all image file paths in one of the source folders
+    image_files = glob(str(ROOT_PATH) + f'/images_320-240_{random.randint(1, 5)}/*/*/Color/*/*.jp*g')
 
     while True:
+        # Get a random image from the set of allowed labels
         img_path = np.random.choice(image_files)
         img_original = cv2.imread(img_path)
-        print(f'Using image: {img_path}')
+
+        # Create trackbars
+        cv2.namedWindow('Trackbars', cv2.WINDOW_AUTOSIZE)
+        cv2.createTrackbar('Min Alpha', 'Trackbars', AMIN, 255, do_nothing)
+        cv2.createTrackbar('Min Beta', 'Trackbars', BMIN, 255, do_nothing)
+        cv2.createTrackbar('Max Alpha', 'Trackbars', AMAX, 255, do_nothing)
+        cv2.createTrackbar('Max Beta', 'Trackbars', BMAX, 255, do_nothing)
+        cv2.createTrackbar('Binary Open/Close Size', 'Trackbars', BINARY_OPEN_CLOSE_SIZE, 30, do_nothing)
+        cv2.createTrackbar('Canny Blur', 'Trackbars', CANNY_BLUR_SIZE, 10, do_nothing)
+        a_min, a_max, b_min, b_max, binary_kernel_size, canny_blur_size = -1, -1, -1, -1, -1, -1
 
         # Enhance the contrast
         clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
@@ -39,23 +71,34 @@ if __name__ == '__main__':
         gray_bgr = cv2.cvtColor(gray, cv2.COLOR_GRAY2BGR)
         row1 = np.hstack((img_original, img, lab, gray_bgr))
 
-        # Create trackbars
-        cv2.namedWindow('Trackbars', cv2.WINDOW_AUTOSIZE)
-        cv2.createTrackbar('a_min', 'Trackbars', AMIN, 255, lambda x: x)
-        cv2.createTrackbar('b_min', 'Trackbars', BMIN, 255, lambda x: x)
-        cv2.createTrackbar('a_max', 'Trackbars', AMAX, 255, lambda x: x)
-        cv2.createTrackbar('b_max', 'Trackbars', BMAX, 255, lambda x: x)
-        cv2.createTrackbar('erosion', 'Trackbars', EROSION_SIZE, 30, lambda x: x)
-        cv2.createTrackbar('blur', 'Trackbars', BLUR_SIZE, 10, lambda x: x)
-
         while True:
+            # Update previous value
+            a_min_prev = a_min
+            a_max_prev = a_max
+            b_min_prev = b_min
+            b_max_prev = b_max
+            binary_kernel_size_prev = binary_kernel_size
+            blur_prev = canny_blur_size
+
             # Get trackbar values
-            a_min = cv2.getTrackbarPos('a_min', 'Trackbars')
-            b_min = cv2.getTrackbarPos('b_min', 'Trackbars')
-            a_max = cv2.getTrackbarPos('a_max', 'Trackbars')
-            b_max = cv2.getTrackbarPos('b_max', 'Trackbars')
-            erosion_size = make_uneven(cv2.getTrackbarPos('erosion', 'Trackbars'))
-            blur_size = make_uneven(cv2.getTrackbarPos('blur', 'Trackbars'))
+            a_min = cv2.getTrackbarPos('Min Alpha', 'Trackbars')
+            b_min = cv2.getTrackbarPos('Min Beta', 'Trackbars')
+            a_max = cv2.getTrackbarPos('Max Alpha', 'Trackbars')
+            b_max = cv2.getTrackbarPos('Max Beta', 'Trackbars')
+            binary_kernel_size = make_uneven(cv2.getTrackbarPos('Binary Open/Close Size', 'Trackbars'))
+            canny_blur_size = make_uneven(cv2.getTrackbarPos('Canny Blur', 'Trackbars'))
+
+            # Detect a change in the trackbars
+            parameters_changed = any(
+                [
+                    a_min != a_min_prev,
+                    a_max != a_max_prev,
+                    b_min != b_min_prev,
+                    b_max != b_max_prev,
+                    binary_kernel_size != binary_kernel_size_prev,
+                    canny_blur_size != blur_prev
+                ]
+            )
 
             # Mask the skin color
             skin_color_mask = cv2.inRange(lab, (LMIN, a_min, b_min), (LMAX, a_max, b_max))
@@ -63,53 +106,72 @@ if __name__ == '__main__':
 
             # Binarize the image
             ret, gray_binary = cv2.threshold(gray_masked, 1, 255, cv2.THRESH_BINARY)
-            kernel = np.ones((erosion_size, erosion_size), np.uint8)
+            kernel = np.ones((BINARY_OPEN_CLOSE_SIZE, BINARY_OPEN_CLOSE_SIZE), np.uint8)
             gray_binary = cv2.morphologyEx(gray_binary, cv2.MORPH_OPEN, kernel)
             gray_binary = cv2.morphologyEx(gray_binary, cv2.MORPH_CLOSE, kernel)
 
             # Blur the image before edge detection
-            gray_blurred = cv2.GaussianBlur(gray, (blur_size, blur_size), sigmaX=2)
-            v = np.median(gray)
-            sigma = 0.33
-            lower = int(max(0, (1.0 - sigma) * v))
-            upper = int(min(255, (1.0 + sigma) * v))
+            gray_blurred = cv2.GaussianBlur(gray, (canny_blur_size, canny_blur_size), sigmaX=2)
 
             # Detect edges in the grayscale image
-            gray_edges = cv2.Canny(gray_blurred, lower, upper, apertureSize=3)
-            kernel = np.ones((closing_size, closing_size), np.uint8)
+            v = np.average(gray)
+            sigma = 0.33
+            lower_canny = int(max(0, (1.0 - sigma) * v))
+            upper_canny = int(min(255, (1.0 + sigma) * v))
+            gray_edges = cv2.Canny(gray_blurred, lower_canny, upper_canny, apertureSize=3)
+            kernel = np.ones((EDGE_DILATION_SIZE, EDGE_DILATION_SIZE), np.uint8)
             gray_edges = cv2.morphologyEx(gray_edges, cv2.MORPH_DILATE, kernel)
 
             # Subtract the edges from the binary image
             gray_edges_inv = cv2.bitwise_not(gray_edges)
             gray_binary = cv2.bitwise_and(gray_binary, gray_edges_inv)
 
-            # Generate a copy of the original within the loop
-            img_box = np.copy(img_original)
+            # Calculate the area limits with respect to the image size
+            min_area, max_area = get_area_thresholds(img_original)
 
             # Find contours in gray image
+            img_box = np.copy(img_original)
             box_cnt = 0
-            contours, hierarchy = cv2.findContours(gray_binary, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-            print(gray_binary.shape)
-            TOTAL_AREA = np.prod(gray_binary.shape[0:2])
-            MAX_AREA = 0.7 * TOTAL_AREA
-            MIN_AREA = 0.03 * TOTAL_AREA
-            print(f'Area of boxes must be within ({MIN_AREA},{MAX_AREA})')
-            print(f'Found {len(contours)} contours')
-            if len(contours) > 1:
-                min_area_act = min(cv2.contourArea(cnt) for cnt in contours)
-                max_area_act = max(cv2.contourArea(cnt) for cnt in contours)
-                print(f'Minimum area found {min_area_act}, maximum area found {max_area_act}')
+            contours, hierarchy = cv2.findContours(gray_edges, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+            cv2.drawContours(img_box, contours, -1, (255, 0, 0), thickness=3)
 
-            # Draw contours from binary image
-            cv2.drawContours(img_box, contours, -1, (255, 0, 0), 3)
-            for cnt in contours:
+            # Iterate over the contours to find the correct ones
+            for i, cnt in enumerate(contours):
+                # Calculate the area of the current contour
                 area = cv2.contourArea(cnt)
+                valid_area = max_area > area > min_area
+
+                # Calculate the fill factor of the contour
+                filled_contour = np.zeros(img_box.shape[0:2], dtype=np.uint8)
+                cv2.drawContours(filled_contour, contours, i, 255, thickness=cv2.FILLED)
+                total_pixels = np.count_nonzero(filled_contour)
+
+                # Get the pixels that are both within the contour and in the binary image
+                valid_fill_ratio = False
+                if total_pixels > 200:
+                    filled_contour = cv2.bitwise_and(filled_contour, gray_binary)
+                    filled_pixels = np.count_nonzero(filled_contour)
+                    fill_ratio = filled_pixels / total_pixels
+                    valid_fill_ratio = fill_ratio > MIN_FILL_RATIO
+
                 # Draw bounding box if area of contour is sufficient
-                if MAX_AREA > area > MIN_AREA:
+                if valid_area and valid_fill_ratio:
+                    box_cnt += 1
                     x, y, w, h = cv2.boundingRect(cnt)
                     cv2.rectangle(img_box, (x, y), (x + w, y + h), (0, 255, 0), thickness=2)
-                    box_cnt += 1
-                    print(f'Found {box_cnt} boxes so far')
+                    cv2.imshow(f'Filled Contour AND Binary Image ({box_cnt})', filled_contour)
+
+            # Debugging information
+            if parameters_changed:
+                print(80 * '=')
+                print('Debug information:')
+                print(f'Using image: {img_path}')
+                print(f'Found {len(contours)} contours in blurred gray-scale.')
+                contours2, _ = cv2.findContours(gray_binary, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+                print(f'Found {len(contours)} contours in binary image.')
+                print(f'Canny Avg {v:.3f}, Sigma {sigma:.3f}')
+                print(f'Canny thresholds: {int(max(0, (1.0 - sigma) * v))};{int(min(255, (1.0 + sigma) * v))}')
+                print(80 * '=')
 
             row2 = np.hstack((
                 cv2.cvtColor(gray_masked, cv2.COLOR_GRAY2BGR),
