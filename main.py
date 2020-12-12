1e5d3558cc6c27efab5cd3bdb339827d7fa1179b
Patrick Bertsch <47899331+pat-bert@users.noreply.github.com>
2020-12-12 20:13:53 +0100

    Updating labels Main uses multiprocessing Using early stop callback and avg pooling instead of max pooling

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
+import multiprocessing as mp
 from glob import glob
+from pathlib import Path
 
 import cv2
 import numpy as np
@@ -7,12 +8,10 @@ from skimage import color, segmentation, img_as_ubyte
 from skimage.future import graph
 from skimage.segmentation import mark_boundaries
 
-from check_label import ROOT_PATH
-
 # Skin Color Mask
 LMIN, LMAX = 0, 255
-AMIN, AMAX = 131, 165
-BMIN, BMAX = 132, 214
+AMIN, AMAX = 126, 165
+BMIN, BMAX = 128, 214
 
 # Filter Kernel Sizes
 EDGE_DILATION_SIZE = 3
@@ -65,11 +64,6 @@ def merge_mean_color(graph, src, dst):
     graph.nodes[dst]['mean color'] = (graph.nodes[dst]['total color'] / graph.nodes[dst]['pixel count'])
 
 
-def make_uneven(val):
-    val = max(3, val)
-    return val if val % 2 == 1 else val + 1
-
-
 def get_area_thresholds(image):
     """
     Calculate the absolute area thresholds with respect to the total image area
@@ -84,129 +78,62 @@ def do_nothing(_):
     pass
 
 
+def preprocess(file):
+    # Get x random image from the set of allowed labels
+    img_original = cv2.imread(file)
+
+    # Enhance the contrast
+    clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
+    img_hsv = cv2.cvtColor(img_original, cv2.COLOR_BGR2HSV)
+    img_hsv[:, :, 2] = clahe.apply(img_hsv[:, :, 2])
+    img = cv2.cvtColor(img_hsv, cv2.COLOR_HSV2BGR)
+
+    # Transform to other color spaces
+    lab = cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
+
+    # Mask the skin color
+    skin_color_mask = cv2.inRange(lab, (LMIN, AMIN, BMIN), (LMAX, AMAX, BMAX))
+
+    # Segmentation
+    img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    labels = segmentation.slic(img_rgb, n_segments=250, start_label=1, slic_zero=True)
+    # print(f"SLIC number of segments: {len(np.unique(labels))}")
+    out = img_as_ubyte(mark_boundaries(img_rgb.copy(), labels))
+    img_segmented = cv2.cvtColor(out, cv2.COLOR_RGB2BGR)
+
+    # Graph merging
+    img_rgb_masked = cv2.bitwise_and(img_rgb, img_rgb, mask=skin_color_mask)
+    g = graph.rag_mean_color(img_rgb_masked, labels)
+    labels2 = graph.merge_hierarchical(labels, g, thresh=40, rag_copy=False, in_place_merge=True,
+                                       merge_func=merge_mean_color, weight_func=_weight_mean_color)
+    out = color.label2rgb(labels2, img_rgb_masked, kind='avg', bg_label=0)
+    out = img_as_ubyte(mark_boundaries(out, labels2, (0, 0, 0)))
+    img_merged = cv2.cvtColor(out, cv2.COLOR_RGB2BGR)
+
+    # Convert to mask and apply to original image
+    grayscale = cv2.cvtColor(img_merged, cv2.COLOR_BGR2GRAY)
+    thresh = cv2.inRange(grayscale, 10, 255)
+    thresh = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, np.ones(5))
+    thresh = cv2.dilate(thresh, np.ones(3))
+    masked_img = cv2.bitwise_and(img, img, mask=thresh)
+
+    # row1 = np.hstack((img_original, image))
+    # row2 = np.hstack((img_segmented, img_merged))
+    # row3 = np.hstack((cv2.cvtColor(thresh, cv2.COLOR_GRAY2BGR), masked_img))
+    # block = np.vstack((row1, row2, row3))
+    # cv2.imshow('Original and CLAHE enhanced |Segmented and Merged | Binary and Masked', block)
+    # cv2.destroyAllWindows()
+
+    # Save images
+    new_filepath = Path(file.replace('ressource_rgb', 'ressource_slic'))
+    new_filepath.parent.mkdir(parents=True, exist_ok=True)
+    cv2.imshow(str(new_filepath), masked_img)
+    cv2.waitKey()
+
+
 if __name__ == '__main__':
     # Get all image file paths in one of the source folders
-    image_files = glob(str(ROOT_PATH) + f'/images_320-240_{random.randint(1, 5)}/*/*/Color/*/*.jp*g')
+    image_files = glob(r'D:\Nutzer\Documents\PycharmProjects\crv\ressource_rgb\*\*\*.jp*g')
 
-    while True:
-        # Get a random image from the set of allowed labels
-        img_path = np.random.choice(image_files)
-        img_original = cv2.imread(img_path)
-
-        # Create trackbars
-        cv2.namedWindow('Trackbars', cv2.WINDOW_AUTOSIZE)
-        cv2.createTrackbar('Min Alpha', 'Trackbars', AMIN, 255, do_nothing)
-        cv2.createTrackbar('Min Beta', 'Trackbars', BMIN, 255, do_nothing)
-        cv2.createTrackbar('Max Alpha', 'Trackbars', AMAX, 255, do_nothing)
-        cv2.createTrackbar('Max Beta', 'Trackbars', BMAX, 255, do_nothing)
-        cv2.createTrackbar('SLIC Thresh', 'Trackbars', 30, 255, do_nothing)
-
-        # Enhance the contrast
-        clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
-        img_hsv = cv2.cvtColor(img_original, cv2.COLOR_BGR2HSV)
-        img_hsv[:, :, 2] = clahe.apply(img_hsv[:, :, 2])
-        img = cv2.cvtColor(img_hsv, cv2.COLOR_HSV2BGR)
-
-        # Transform to other color spaces
-        lab = cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
-
-        while True:
-            # Get trackbar values
-            a_min = cv2.getTrackbarPos('Min Alpha', 'Trackbars')
-            b_min = cv2.getTrackbarPos('Min Beta', 'Trackbars')
-            a_max = cv2.getTrackbarPos('Max Alpha', 'Trackbars')
-            b_max = cv2.getTrackbarPos('Max Beta', 'Trackbars')
-            slic_thresh = cv2.getTrackbarPos('SLIC Thresh', 'Trackbars')
-
-            # Mask the skin color
-            skin_color_mask = cv2.inRange(lab, (LMIN, a_min, b_min), (LMAX, a_max, b_max))
-
-            # Segmentation
-            img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-            labels = segmentation.slic(img_rgb, n_segments=250, start_label=1, slic_zero=True)
-            print(f"SLIC number of segments: {len(np.unique(labels))}")
-            out = img_as_ubyte(mark_boundaries(img_rgb.copy(), labels))
-            img_segmented = cv2.cvtColor(out, cv2.COLOR_RGB2BGR)
-
-            # Graph merging
-            img_rgb_masked = cv2.bitwise_and(img_rgb, img_rgb, mask=skin_color_mask)
-            g = graph.rag_mean_color(img_rgb_masked, labels)
-            labels2 = graph.merge_hierarchical(labels, g, thresh=slic_thresh, rag_copy=False, in_place_merge=True,
-                                               merge_func=merge_mean_color, weight_func=_weight_mean_color)
-            out = color.label2rgb(labels2, img_rgb_masked, kind='avg', bg_label=0)
-            out = img_as_ubyte(mark_boundaries(out, labels2, (0, 0, 0)))
-            img_merged = cv2.cvtColor(out, cv2.COLOR_RGB2BGR)
-
-            row1 = np.hstack((img_original, img))
-            row2 = np.hstack((img_segmented, img_merged))
-            block = np.vstack((row1, row2))
-            cv2.imshow('Original and CLAHE enhanced |Segmented and Merged', block)
-
-            # # Binarize the image
-            # ret, gray_binary = cv2.threshold(gray_masked, 1, 255, cv2.THRESH_BINARY)
-            # kernel = np.ones((BINARY_OPEN_CLOSE_SIZE, BINARY_OPEN_CLOSE_SIZE), np.uint8)
-            # gray_binary = cv2.morphologyEx(gray_binary, cv2.MORPH_OPEN, kernel)
-            # gray_binary = cv2.morphologyEx(gray_binary, cv2.MORPH_CLOSE, kernel)
-            #
-            # # Calculate the area limits with respect to the image size
-            # min_area, max_area = get_area_thresholds(img_original)
-
-            # # Find contours in gray image
-            # img_box = np.copy(img_original)
-            # box_cnt = 0
-            # contours, hierarchy = cv2.findContours(gray_edges, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-            # cv2.drawContours(img_box, contours, -1, (255, 0, 0), thickness=3)
-            #
-            # # Iterate over the contours to find the correct ones
-            # for i, cnt in enumerate(contours):
-            #     # Calculate the area of the current contour
-            #     area = cv2.contourArea(cnt)
-            #     valid_area = max_area > area > min_area
-            #
-            #     # Calculate the fill factor of the contour
-            #     filled_contour = np.zeros(img_box.shape[0:2], dtype=np.uint8)
-            #     cv2.drawContours(filled_contour, contours, i, 255, thickness=cv2.FILLED)
-            #     total_pixels = np.count_nonzero(filled_contour)
-            #
-            #     # Get the pixels that are both within the contour and in the binary image
-            #     valid_fill_ratio = False
-            #     if total_pixels > 200:
-            #         filled_contour = cv2.bitwise_and(filled_contour, gray_binary)
-            #         filled_pixels = np.count_nonzero(filled_contour)
-            #         fill_ratio = filled_pixels / total_pixels
-            #         valid_fill_ratio = fill_ratio > MIN_FILL_RATIO
-            #
-            #     # Draw bounding box if area of contour is sufficient
-            #     if valid_area and valid_fill_ratio:
-            #         box_cnt += 1
-            #         x, y, w, h = cv2.boundingRect(cnt)
-            #         cv2.rectangle(img_box, (x, y), (x + w, y + h), (0, 255, 0), thickness=2)
-            #         cv2.imshow(f'Filled Contour AND Binary Image ({box_cnt})', filled_contour)
-            #
-            # # Debugging information
-            # if parameters_changed:
-            #     print(80 * '=')
-            #     print('Debug information:')
-            #     print(f'Using image: {img_path}')
-            #     print(f'Found {len(contours)} contours in blurred gray-scale.')
-            #     contours2, _ = cv2.findContours(gray_binary, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-            #     print(f'Found {len(contours)} contours in binary image.')
-            #     print(80 * '=')
-            #
-            # row2 = np.hstack((
-            #     cv2.cvtColor(gray_masked, cv2.COLOR_GRAY2BGR),
-            #     cv2.cvtColor(gray_binary, cv2.COLOR_GRAY2BGR),
-            #     cv2.cvtColor(gray_edges, cv2.COLOR_GRAY2BGR),
-            #     img_box
-            # ))
-            # res = np.vstack((row1, row2))
-            # cv2.imshow('<Original | Original CLAHE | LAB CLAHE | Gray CLAHE> <Skin Masked | Binary | Edges | Boxes>',
-            #            res)
-
-            k = cv2.waitKey(1) & 0xFF
-            if k == 27:
-                break
-
-        # Prepare region of interest of hand for neuronal network
-
-        cv2.destroyAllWindows()
+    pool = mp.Pool(processes=1)
+    pool.map(preprocess, image_files)
