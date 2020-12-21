ae530130e425d764917fbe1db931f438ff6b7665
Patrick Bertsch <47899331+pat-bert@users.noreply.github.com>
2020-12-21 12:08:34 +0100

    Adding plot for confusion matrix

--- Neuronales_Netz_Test.py
+++ Neuronales_Netz_Test.py
@@ -1,5 +1,6 @@
+import matplotlib.pyplot as plt
 import numpy as np
-from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.metrics import classification_report, confusion_matrix, ConfusionMatrixDisplay
 from tensorflow.keras.applications.nasnet import preprocess_input
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
@@ -20,7 +21,12 @@ if __name__ == '__main__':
     # Confution Matrix and Classification Report
     Y_pred = model.predict(test_data)
     y_pred = np.argmax(Y_pred, axis=1)
-    print('Confusion Matrix')
-    print(confusion_matrix(test_data.classes, y_pred))
+    print('Confusion Matrix (True Label as row, predicted label as column)')
+    cm = confusion_matrix(test_data.classes, y_pred)
+    print(cm)
     print('Classification Report')
     print(classification_report(test_data.classes, y_pred))
+    disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=test_data.class_indices.keys())
+    disp.plot(include_values=True, cmap=plt.cm.Blues, xticks_rotation='horizontal')
+    disp.ax_.set_title("Confusion matrix, without normalization")
+    plt.show()
